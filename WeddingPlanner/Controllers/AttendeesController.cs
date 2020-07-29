1d71852346b36fe133cd985842027c5157ebaf7c
Jordan Henry <jordanhenry92@hotmail.com>
2020-07-29 15:56:15 -0400

    Added page to display all attendees

---
+++ WeddingPlanner/Controllers/AttendeesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using WeddingPlanner.Models;
+
+namespace WeddingPlanner.Controllers
+{
+    public class AttendeesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public AttendeesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // Override base dispose method for our DbContext
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Attendees
+        public ActionResult Index()
+        {
+            var attendees = _context.Attendees
+                .Include(a => a.Role)
+                .ToList();
+
+            return View(attendees);
+        }
+    }
+}
