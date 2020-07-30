aca6508f193765205449160cbee590683cbc7383
Jordan Henry <jordanhenry92@hotmail.com>
2020-07-29 20:16:27 -0400

    Added ability to edit an existing attendee and modified some styling

--- WeddingPlanner/Controllers/AttendeesController.cs
+++ WeddingPlanner/Controllers/AttendeesController.cs
@@ -5,12 +5,13 @@ using System.Web;
 using System.Data.Entity;
 using System.Web.Mvc;
 using WeddingPlanner.Models;
+using WeddingPlanner.ViewModels;
 
 namespace WeddingPlanner.Controllers
 {
     public class AttendeesController : Controller
     {
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public AttendeesController()
         {
@@ -23,7 +24,7 @@ namespace WeddingPlanner.Controllers
             _context.Dispose();
         }
 
-        // GET: Attendees
+        // Returns a list of attendees in the db
         public ActionResult Index()
         {
             var attendees = _context.Attendees
@@ -31,6 +32,55 @@ namespace WeddingPlanner.Controllers
                 .ToList();
 
             return View(attendees);
+        }
+
+        // Returns a form that allows a user to edit an attendee's details
+        public ActionResult Edit(int id)
+        {
+            var attendee = _context.Attendees.SingleOrDefault(a => a.Id == id);
+
+            if (attendee == null)
+                return HttpNotFound();
+
+            var viewModel = new AttendeeFormViewModel
+            {
+                Attendee = attendee,
+                Roles = _context.Roles.ToList()
+            };
+
+            return View("AttendeeForm", viewModel);
+        }
+
+        // Saves an edited attendee to the database and returns user to attendees list
+        [HttpPost]
+        public ActionResult Save(Attendee attendee)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new AttendeeFormViewModel
+                {
+                    Attendee = attendee,
+                    Roles = _context.Roles
+                };
+
+                return View("AttendeeForm", viewModel);
+            }
+
+            if (attendee.Id == 0)
+                _context.Attendees.Add(attendee);
+            else
+            {
+                var attendeeInDb = _context.Attendees.SingleOrDefault(a => a.Id == attendee.Id);
+
+                attendeeInDb.FirstName = attendee.FirstName;
+                attendeeInDb.LastName = attendee.LastName;
+                attendeeInDb.Email = attendee.Email;
+                attendeeInDb.PhoneNumber = attendee.PhoneNumber;
+                attendeeInDb.RoleId = attendee.RoleId;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Attendees");
         }
     }
 }
---
+++ WeddingPlanner/ViewModels/AttendeeFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeddingPlanner.Models;
+
+namespace WeddingPlanner.ViewModels
+{
+    public class AttendeeFormViewModel
+    {
+        public Attendee Attendee { get; set; }
+        public IEnumerable<Role> Roles { get; set; }
+    }
+}
